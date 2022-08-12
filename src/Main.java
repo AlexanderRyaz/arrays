c32556d623c381342b924d0ed40edd7d26a24986
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-08-12 14:11:33 +0300

    задание 3 исправил

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         int[] arrays = new int[3];
         arrays[0] = 1; arrays[1] = 2; arrays[2] = 3;
-        System.out.println(arrays[0] + "," + arrays[1] + "," + arrays[2]);
+        System.out.println(arrays[2] + "," + arrays[1] + "," + arrays[0]);
 
         double[] doubleArr = new double[] {1.57, 7.654, 9.986};
-        System.out.println(doubleArr[0] + "," + doubleArr[1] + "," + doubleArr[2]);
+        System.out.println(doubleArr[2] + "," + doubleArr[1] + "," + doubleArr[0]);
 
         String[] names = {"Таня", "Ника", "Саша"};
-        System.out.print(names[0] + "," + names[1] + "," + names[2]);
+        System.out.print(names[2] + "," + names[1] + "," + names[0]);
             }
         }
 
