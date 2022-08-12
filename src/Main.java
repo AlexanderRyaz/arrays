d5f0297dcc24a5d24a415a724a0060ca89ff35a1
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-08-12 11:00:02 +0300

    3е готово

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,19 @@ public class Main {
     public static void main(String[] args) {
         int[] arrays = new int[3];
         arrays[0] = 1; arrays[1] = 2; arrays[2] = 3;
-        for (int i = 0; i < arrays.length; i++) {
+        for (int i = arrays.length - 1; i >= 0; i--) {
             System.out.print(arrays[i] + ","); }
 
         System.out.println();
 
         double[] doubleArr = new double[] {1.57, 7.654, 9.986};
-        for (int i = 0; i < doubleArr.length; i++) {
-            System.out.print(doubleArr[i] + ","); }
+        for (int i = doubleArr.length - 1; i >= 0; i--) {
+            System.out.print(doubleArr[i]  + ","); }
 
         System.out.println();
 
         String[] names = {"Таня", "Ника", "Саша"};
-        for (int i = 0; i < names.length; i++) {
+        for (int i = names.length - 1; i >= 0; i--) {
             System.out.print(names[i] + ","); }
 
 
