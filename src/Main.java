9753b36b8119d15bbd2594b28bfe142bd99208c3
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-08-12 10:45:20 +0300

    половина сделана

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arrays = new int[3];
+        arrays[0] = 1; arrays[1] = 2; arrays[2] = 3;
+        for (int i = 0; i < arrays.length; i++) {
+            System.out.print(arrays[i] + ","); }
+
+        System.out.println();
+
+        double[] doubleArr = new double[] {1.57, 7.654, 9.986};
+        for (int i = 0; i < doubleArr.length; i++) {
+            System.out.print(doubleArr[i] + ","); }
+
+        System.out.println();
+
+        String[] names = {"Таня", "Ника", "Саша"};
+        for (int i = 0; i < names.length; i++) {
+            System.out.print(names[i] + ","); }
+
+
+
     }
 }
